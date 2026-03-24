59c3be9c6fe2ae31e62e77668c2bd3d50239e876
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-03-23 21:07:27 -0300

    :sparkles: feat: add products in progress (script.js)

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+let listProducts = [
+    {
+        name: "Hamburguer",
+        description: "A delicious hamburguer",
+        price: 22.00,
+        photoURL: "https://videocdn.cdnpk.net/videos/6106416e-a9f7-5305-8067-fe819182f8b7/horizontal/thumbnails/small.jpg?item_id=5036298"
+    }
+]
+
+const prodList = document.getElementById("prod-list");
+
+const productName = document.getElementById("name");
+const productDescription = document.getElementById("description");
+const productPrice = document.getElementById("price");
+const productPhotoURL = document.getElementById("photo-url")
+const botao = document.getElementById("btn")
+
+botao.addEventListener("click", (e) => {
+    e.preventDefault();
+    listProducts.push(
+        {
+            name: productName.value,
+            description: productDescription.value,
+            price: productPrice.value,
+            photoURL: productPhotoURL.value
+        });
+
+        prodList.innerHTML = "";
+        console.clear();
+
+    for (let i = 0; i < listProducts.length; i++) {
+        const li = document.createElement("li");
+        console.log(listProducts[i]);
+        li.innerText = ("<Name: " + listProducts[i].name + "\nDescription: " + listProducts[i].description + "\nPrice: " + listProducts[i].price )
+        prodList.appendChild(li);
+    };
+
+    productName.value = "";
+    productDescription.value = "";
+    productPrice.value = "";
+    productPhotoURL.value = "";
+})
