5f9587f88cd25a83909261fc9915f6ab31834f9c
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-03-27 17:20:12 -0300

    :lipstick: style: updating css and html of feture/home

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,48 @@
-let listProducts = [
-    {
-        name: "Hamburguer",
-        id: 1,
-        description: "A delicious hamburguer",
-        price: 22.00,
-        photoURL: "https://videocdn.cdnpk.net/videos/6106416e-a9f7-5305-8067-fe819182f8b7/horizontal/thumbnails/small.jpg?item_id=5036298"
-    },
-    {
-        name: "pizza",
-        id: 2,
-        description: "A delicious pizza",
-        price: 21.00,
-        photoURL: "https://videocdn.cdnpk.net/videos/6106416e-a9f7-5305-8067-fe819182f8b7/horizontal/thumbnails/small.jpg?item_id=5036298"
-    },
-    {
-        name: "peixe",
-        id: 3,
-        description: "A delicious peixe",
-        price: 20.00,
-        photoURL: "https://videocdn.cdnpk.net/videos/6106416e-a9f7-5305-8067-fe819182f8b7/horizontal/thumbnails/small.jpg?item_id=5036298"
-    }
-]
+// let listProducts = [
+//     {
+//         name: "Hamburguer",
+//         id: 1,
+//         description: "A delicious hamburguer",
+//         price: 22.00,
+//         photoURL: "https://videocdn.cdnpk.net/videos/6106416e-a9f7-5305-8067-fe819182f8b7/horizontal/thumbnails/small.jpg?item_id=5036298"
+//     },
+//     {
+//         name: "pizza",
+//         id: 2,
+//         description: "A delicious pizza",
+//         price: 21.00,
+//         photoURL: "https://videocdn.cdnpk.net/videos/6106416e-a9f7-5305-8067-fe819182f8b7/horizontal/thumbnails/small.jpg?item_id=5036298"
+//     },
+//     {
+//         name: "peixe",
+//         id: 3,
+//         description: "A delicious peixe",
+//         price: 20.00,
+//         photoURL: "https://videocdn.cdnpk.net/videos/6106416e-a9f7-5305-8067-fe819182f8b7/horizontal/thumbnails/small.jpg?item_id=5036298"
+//     }
+// ]
 
-listProducts.forEach(prod => {
-    const prodList = document.getElementById("prod-list");
-    const orderList = document.getElementById("order-list")
-    const btnEnd = document.getElementById("btn")
+// listProducts.forEach(prod => {
+//     const prodList = document.getElementById("prod-list");
+//     const orderList = document.getElementById("order-list")
+//     const btnEnd = document.getElementById("btn")
 
-    const liProd = document.createElement("li");
-    const liOrd = document.createElement("li");
-    const btnBuy = document.createElement("button");
+//     const liProd = document.createElement("li");
+//     const liOrd = document.createElement("li");
+//     const btnBuy = document.createElement("button");
 
-    liProd.innerText = ("Product name: \n" + prod.name + "\n$: " + prod.price);
-    btnBuy.innerHTML = "<button id=\"btnBuy\">btnBuy</button>";
-    btnBuy.innerText = ("Buy");
+//     liProd.innerText = ("Product name: \n" + prod.name + "\n$: " + prod.price);
+//     btnBuy.innerHTML = "<button id=\"btnBuy\">btnBuy</button>";
+//     btnBuy.innerText = ("Buy");
 
-    liProd.appendChild(btnBuy);
-    prodList.appendChild(liProd);
+//     liProd.appendChild(btnBuy);
+//     prodList.appendChild(liProd);
 
-    btnBuy.addEventListener("click", () => {
-        liOrd.innerText = ("Product name:   " + prod.name + "  $: " + prod.price)
-        orderList.appendChild(liOrd);
-        orderList.appendChild(btnEnd);
-    })
+//     btnBuy.addEventListener("click", () => {
+//         liOrd.innerText = ("Product name:   " + prod.name + "  $: " + prod.price)
+//         orderList.appendChild(liOrd);
+//         orderList.appendChild(btnEnd);
+//     })
 
-});
+// });
 
