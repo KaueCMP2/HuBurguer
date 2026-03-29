333fad06d4d74445ef452550730d331cf045bd09
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-03-29 17:29:31 -0300

    :lipstick: style: updating style and html page

---
+++ scripts/sign.js
@@ -0,0 +1,65 @@
+let listUsers = [
+    {
+        name: "admin",
+        email: "admin@admin.com",
+        password: 123456,
+    },
+    {
+        name: "Kaue Sergio",
+        email: "kaue@email.com",
+        password: 123456,
+    },
+    {
+        name: "Joao",
+        email: "joao@email.com",
+        password: 123456,
+    },
+    {
+        name: "Algordan",
+        email: "algordan@email.com",
+        password: 123456,
+    },
+    {
+        name: "Apployaldo",
+        email: "apployaldo@email.com",
+        password: 123456,
+    }
+]
+
+const botao = document.getElementById("btn");
+const userName = document.getElementById("name");
+const userMail = document.getElementById("mail");
+const userPass = document.getElementById("pass");
+
+let RealyPass = "";
+const contador = 0;
+
+botao.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let emailCadastrado = listUsers.some(um => um.email == userMail.value);
+    if (emailCadastrado) {
+        alert("Email já cadastrado");
+        return
+    }
+
+    else if (userPass.value == null || userPass.value == "" || userName.value == null || userPass.value == "" || userMail.value == null || userMail.value == "") {
+        alert("Preencha todos os campos");
+        return;
+    }
+
+    listUsers.push(
+        {
+            name: userName.value,
+            email: userMail.value,
+            password: userPass.value
+        }
+    );
+    alert("Olá " + userName.value + " seja bem vindo!!!");
+    cadastrado = false;
+    console.log(listUsers)
+    
+    userName.value = "";
+    userMail.value = "";
+    userPass.value = "";
+});
